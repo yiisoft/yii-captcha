0c1a1d09bffac531705914568536a63e60c7e197
Paul Klimov <klimov.paul@gmail.com>
2018-02-27 00:43:17 +0200

    basic code infrastructure created

---
+++ tests/VerifyCodeGeneratorTraitTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yiiunit\captcha;
+
+use yii\captcha\Driver;
+
+class VerifyCodeGeneratorTraitTest extends TestCase
+{
+    public function testGenerateVerifyCode()
+    {
+        /* @var $driver Driver */
+        $driver = $this->getMockBuilder(Driver::class)
+            ->getMockForAbstractClass();
+
+        $this->assertNotEmpty($driver->generateVerifyCode());
+
+        $driver->minLength = 10;
+        $driver->maxLength = 10;
+        $this->assertEquals(10, strlen($driver->generateVerifyCode()));
+    }
+}
