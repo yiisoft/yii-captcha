24d4bfeb85cea806938a423aea409b7df3d22c0e
Alexander Makarov <sam@rmcreative.ru>
2020-11-21 01:52:43 +0300

    Update StyleCI config + fixes

--- src/Driver.php
+++ src/Driver.php
@@ -1,6 +1,9 @@
 <?php
+
+declare(strict_types=1);
 /**
  * @link http://www.yiiframework.com/
+ *
  * @copyright Copyright (c) 2008 Yii Software LLC
  * @license http://www.yiiframework.com/license/
  */
@@ -73,7 +76,7 @@ abstract class Driver extends Component implements DriverInterface
     }
 
     /**
-     * @return null|string
+     * @return string|null
      */
     public function getFontFile(): string
     {
@@ -86,6 +89,7 @@ abstract class Driver extends Component implements DriverInterface
 
     /**
      * @param string $fontFile
+     *
      * @return Driver
      */
     public function setFontFile(string $fontFile): self
--- src/VerifyCodeGeneratorTrait.php
+++ src/VerifyCodeGeneratorTrait.php
@@ -1,6 +1,9 @@
 <?php
+
+declare(strict_types=1);
 /**
  * @link http://www.yiiframework.com/
+ *
  * @copyright Copyright (c) 2008 Yii Software LLC
  * @license http://www.yiiframework.com/license/
  */
@@ -24,8 +27,10 @@ trait VerifyCodeGeneratorTrait
 
     /**
      * Generates new CAPTCHA code.
-     * @return string CAPTCHA code.
+     *
      * @throws \Exception if it was not possible to gather sufficient entropy.
+     *
+     * @return string CAPTCHA code.
      */
     public function generateVerifyCode(): string
     {
